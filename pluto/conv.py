99a88f48bb950505c166f6b8cd14d6d99b86fcfe
mviruch <evensuerbin@126.com>
2019-07-22 01:15:32 +0800

    完善卷积层

--- pluto/conv.py
+++ pluto/conv.py
@@ -2,19 +2,21 @@ import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from PIL import Image
+from globalData import *
 
 from func import *
 
 class conv:
-    def __init(self, w, h):
-        self.pad = 0
-        self.stride = 1
-        
-        self.imSize = (w, h)
-        self.fiSize = (16, 16)
-        
-        self.img = None
+    def __init(self):
+        self.imgs = None
         self.filter = None
 
-    def read(fn):
-        self.img = readPicData('img.png', self.imSize)
+    def convolution(self, fp, extens):
+        self.imgs = reads(fp, extens, imageW, imageH)
+        print self.imgs.shape
+        a = im2col(self.imgs, filterH, filterW, 1, 0)
+        print a.shape
+
+a = conv()
+a.convolution(path.abspath('sample'), 'png')
+

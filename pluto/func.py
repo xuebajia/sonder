90500b2871ba2a41d5f52f879c38dfcec3d5791c
mviruch <evensuerbin@126.com>
2019-07-23 19:14:38 +0800

    更改如下:
    - 添加col2im 函数, 并完善一个bug
    - 完善因提供信息错误导致reads 函数读取不到数据的处理措施

--- pluto/func.py
+++ pluto/func.py
@@ -1,3 +1,4 @@
+# -*- coding: cp936 -*-
 import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
@@ -25,6 +26,9 @@ def reads(path, extens, w, h):
     # datas.shape = (n, c, h, w)
     fns = listdir(path)
     fns = [item for item in fns if item.split('.')[-1] == extens]
+    if len(fns) == 0:
+        raise Exception('open file error in reads(): ', path, extens)
+    
     datas = np.zeros((len(fns), 3, h, w), int)
     for i, item in enumerate(fns):
         datas[i] = readPicData(path + '\\' + item, (w, h))
@@ -36,27 +40,37 @@ def randArray(a, b, *args):
     return np.random.rand(*args) * (b-a) + a
 
 
-def im2col(datas, hF, wF, stride=1, pad=0):
-    # data (n, c, h, w)
+def im2col(inputData, filterH, filterW, stride=1, pad=0):
+    N, C, H, W = inputData.shape
+    # 计算输出数据的大小
+    outH = (2*pad + H - filterH)//stride + 1
+    outW = (2*pad + W - filterW)//stride + 1
 
-    N, C, H, W = datas.shape
-    outH = (2*pad + H - hF)/stride + 1
-    outW = (2*pad + W - wF)/stride + 1
+    # 扩展
+    img = np.pad(inputData, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
 
-    img = np.pad(datas, [(0, 0), (0, 0), (pad, pad), (pad, pad)], 'constant')
-    col = np.zeros((N, C, hF, wF, outH, outW))
-
-    for y in range(hF):
+    col = np.zeros((N, C, filterH, filterW, outH, outW), dtype=int) # <= add dtype=int
+    for y in range(filterH):
         yMax = y + stride*outH
-        for x in range(wF):
+        for x in range(filterW):
             xMax = x + stride*outW
             col[:, :, y, x, :, :] = img[:, :, y:yMax:stride, x:xMax:stride]
 
     col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*outH*outW, -1)
     return col
 
-'''
-x = np.random.rand(3, 3, 4, 4)
-print(im2col(x,2,2,2,0))
-print(im2col(x,2,2,2,0).shape)
-'''
+def col2im(col, inputShape, filterH, filterW, stride=1, pad=0):
+    N, C, H, W = inputShape
+    outH = (H + 2*pad - filterH)//stride + 1
+    outW = (W + 2*pad - filterH)//stride + 1
+    col = col.reshape(N, outH, outW, C, filterH, filterW).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2*pad + stride - 1, W + 2*pad + stride - 1), dtype=int) # <= add dtype=int
+    for y in range(filterH):
+        yMax = y + stride*outH
+        for x in range(filterW):
+            xMax = x + stride*outW
+            # bug here => img[:, :, y:yMax:stride, x:xMax:stride] += col[:, :, y, x, :, :]
+            img[:, :, y:yMax:stride, x:xMax:stride] = col[:, :, y, x, :, :]
+            
+    return img[:, :, pad:H + pad, pad:W + pad]
